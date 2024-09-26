e6da14d845ab266f412c9acb2aa2c6ce7dd5954c
jerry20-01 <jreyesf@ucvvirtual.edu.pe>
2024-09-26 12:31:58 -0500

    Update app.js

--- app.js
+++ app.js
@@ -83,4 +83,13 @@ window.onload = function () {
         checkDates(cronogramaData);
     }
 };
+if ('notification' in window) {
+    Notification.requestPermission().then(permission => {
+        if (permission === 'granted') {
+            console.log('Permission granted for notifications');
+        } else {
+            console.log('Permission denied for notifications');
+        }
+    });
+}
 
